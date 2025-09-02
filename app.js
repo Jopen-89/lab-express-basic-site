63c39416c94ceeb38950beaa421a5c89cf68a8f1
jopen <solimonra@gmail.com>
2025-09-02 16:09:53 +0200

    app creada con usando express

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express();
+
+app.use(express.static('public'))
+
+//primera ruta
+
+app.get('/home', (request, response, next) => 
+    response.sendFile(__dirname + '/views/index.html'));
+
+//ruta para la pagina about
+
+app.get('/aboutButerin', (request, response, next) =>
+    response.sendFile(__dirname + '/views/about.html'));
+
+app.get('/works', (request, response, next) => 
+    response.sendFile(__dirname + '/views/works.html'));
+
+app.listen('3000', () => console.log("la app escuchando en el puerto 3000"))  //con esto express recibe la peticion 
+//conecta servidor interno HTTP de Node con la app Express
